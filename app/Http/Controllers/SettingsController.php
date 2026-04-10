10be3de60e5aa89aac71bb1428ecdfe965920ed0
James Esureña <jesurena@ics.com.ph>
2026-04-10 11:06:59 +0800

    fix: added logs and use DB:transaction for multiple table modification

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -4,22 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Setting;
+use App\Traits\LogQueries;
+use DB;
 
 class SettingsController extends Controller
 {
+    use LogQueries;
     public function update(Request $request)
     {
         $userId = session('userData') ? session('userData')->account_id : 'System';
 
-        foreach ($request->except('_token') as $key => $value) {
-            $setting = Setting::where('key', $key)->first();
+        DB::transaction(function () use ($request, $userId) {
+            foreach ($request->except('_token') as $key => $value) {
+                $setting = Setting::where('key', $key)->first();
 
-            if ($setting) {
-                $setting->value = $value;
-                $setting->updated_by = $userId;
-                $setting->save();
+                if ($setting) {
+                    $setting->value = $value;
+                    $setting->updated_by = $userId;
+                    $setting->save();
+
+                    $this->saveLogs('Updated system setting: ' . $key);
+                }
             }
-        }
+        });
 
         return redirect()->back();
     }
